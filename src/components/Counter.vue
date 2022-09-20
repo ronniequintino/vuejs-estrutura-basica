9a3cfc8505173a3ca3762852ed743e7cdcd02bc0
Ronnie Quintino de Oliveira <ronniequintino@gmail.com>
2022-09-20 19:05:45 -0300

    Ajustes

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>
+            COUNTER: {{ couter }}
+        </h1>
+        <button @click="decrement">
+            Decrement
+        </button>
+        <button @click="increment">
+            Increment
+        </button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Counter',
+        data: () => ({
+            couter: 0
+        }),
+        methods: {
+            decrement: function(){
+                this.counter--
+            },
+            increment: function(){
+                this.counter++
+            }
+        }
+    }
+</script>
